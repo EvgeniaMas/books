f6deaf928b0528ba324b32ea0c2f1522ff0e7f24
Евгения Масликова < >
2017-09-19 21:55:31 +0300

    config change

--- server/config/config.js
+++ server/config/config.js
@@ -2,5 +2,7 @@
 module.exports = {
     // Uncomment to connect with MongoDB on Cloud
     //'url' : 'mongodb://mvc-user-connect:opklnm@ds019028.mlab.com:19028/mvc-app'
-    'url' : 'mongodb://localhost/mvc-app-multimedia'
+    //'url' : 'mongodb://localhost/mvc-app-multimedia'
+
+     'url' : 'mongodb://jane:test>@ds157233.mlab.com:57233/booktrade'
 };
